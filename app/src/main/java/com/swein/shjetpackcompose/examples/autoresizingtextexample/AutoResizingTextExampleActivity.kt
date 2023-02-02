ca05a0ef58f5a78190fd9b68d4c07cf23dbf3075
CodingWithCat <djseokho@vip.qq.com>
2023-02-02 17:14:52 +0900

    Seok Ho

--- app/src/main/java/com/swein/shjetpackcompose/examples/autoresizingtextexample/AutoResizingTextExampleActivity.kt
+++ app/src/main/java/com/swein/shjetpackcompose/examples/autoresizingtextexample/AutoResizingTextExampleActivity.kt
@@ -1,25 +1,20 @@
 package com.swein.shjetpackcompose.examples.autoresizingtextexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
